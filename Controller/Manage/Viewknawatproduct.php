6d38fdfa01c8d7081e6a0602b8956afcb81201a8
Jayjit <jayjit.gatha@tatvasoft.com>
2019-01-01 20:00:42 +0530

    code changes for varnish test issue , #42

---
+++ Controller/Manage/Viewknawatproduct.php
@@ -0,0 +1,45 @@
+<?php
+namespace Knawat\Dropshipping\Controller\Manage;
+
+use \Magento\Framework\App\Action\Action;
+
+/**
+ * Class Viewknawatproduct
+ * @package Knawat\Dropshipping\Controller\Manage
+ */
+class Viewknawatproduct extends Action
+{
+
+    /** @var  \Magento\Framework\View\Result\Page */
+    protected $resultPageFactory;
+
+    /**
+     * @var \Knawat\Dropshipping\Helper\SingleProductUpdate
+     */
+    protected $singleProductHelper;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Knawat\Dropshipping\Helper\SingleProductUpdate $singleProductHelper
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->singleProductHelper = $singleProductHelper;
+        parent::__construct($context);
+    }
+
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     */
+    public function execute()
+    {
+        $sku = $this->getRequest()->getParam('sku');  
+        if($sku){
+            $this->singleProductHelper->runSingleImport($sku);
+        }
+    }
+}
